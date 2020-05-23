2e9098f367b1dab9ddb3b447045ed328f8699cb5
madhavsharma2106 <madhavsharma2106@gmail.com>
2020-05-23 13:43:15 +0530

    Chart is now responsive

--- src/components/organism/Chart/Chart.jsx
+++ src/components/organism/Chart/Chart.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+} from "recharts";
 
 function Chart({ data }) {
   const renderLineChart = (
-    <LineChart width={600} height={300} data={data}>
-      <XAxis dataKey="id" />
-      <YAxis />
-      <CartesianGrid strokeDasharray="3 3" />
-
-      <Line type="monotone" dataKey="votes" stroke="#ff6500" />
-    </LineChart>
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart data={data}>
+        <XAxis dataKey="id" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Line type="monotone" dataKey="votes" stroke="#ff6500" />
+      </LineChart>
+    </ResponsiveContainer>
   );
   return renderLineChart;
 }
