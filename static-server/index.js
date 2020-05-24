2bebf722bf9f6cb920953ddeb130f5f066044faa
madhavsharma2106 <madhavsharma2106@gmail.com>
2020-05-25 01:27:43 +0530

    Static server now serves all paths

--- static-server/index.js
+++ static-server/index.js
@@ -3,7 +3,13 @@ const path = require("path");
 const app = express();
 const PORT = 8000;
 
-app.use(express.static(path.resolve(__dirname, "..", "build")));
+// Serve any static files
+app.use(express.static(path.join(__dirname, "../build")));
+
+// Handle React routing, return all requests to React app
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`APP launched on ${PORT}`);
